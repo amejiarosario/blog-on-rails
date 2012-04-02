9b62a5df8901aef09aab75046559a312347a9166
Adrian Mejia <axm7988@rit.edu>
2012-04-01 22:35:24 -0400

    blog working properly (no test though)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment])
+    flash[:error] = @comment.errors unless @comment.save
     redirect_to post_path(@post)
   end
   
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
+  validates :commenter, presence: true
+  validates :body, presence: true
+  
   belongs_to :post
 end
