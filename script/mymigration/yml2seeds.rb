6e963396bf2edac7ab8a7320ad69fa8b7d5a0a49
Adrian Mejia <axm7988@rit.edu>
2012-04-09 01:17:31 -0400

    tags included in the migration

--- script/mymigration/yml2seeds.rb
+++ script/mymigration/yml2seeds.rb
@@ -1,22 +1,43 @@
 require 'yaml'
+require 'set'
 
 def validate(html)
-  #html = html.gsub(/<img.*src="([^"]*)".*>/) { |m| m.gsub!($1,"http://adrianmejiarosario.com#{$1}")  }
-  #html = html.gsub(/<img.*(src=")\/sites\/default\/files\/pictures\/[^"]*"[^>]*>/){|m| m.gsub!($1,"#{$1}http://adrianmejiarosario.com")}
   html = html.gsub("\"/sites/default/files/", "\"http://adrianmejiarosario.com/sites/default/files/")
   html = html.gsub("'","\\\\'")
 end
+
+def tag_var (tag)
+  "tag_"+tag.gsub(/[^a-zA-Z0-9]/,"_")
+  #tag.gsub("-","_").gsub(/\s+/,"_").gsub('#',"_").gsub('.',"_").gsub('(',"_")
+end
+
+tags_set = Set.new
 
 yml = open('dr_node.yml') { |f| YAML.load f }
 
 File.open('output_seed.rb','w') do |f|
   # create all posts
-  yml.each do |p|
-    f.write "adrian.posts.create("+
+  posts = ""
+  tags = ""
+  yml.each_with_index do |p,i|
+    posts += "p#{i} = adrian.posts.create("+
     "title: '#{p['title'].gsub("'","\\\\'")}', "+
     "content: '#{validate(p['body_value'])}', " +
     "created_at: '#{Time.at(p['created'])}', " +
     "updated_at: '#{Time.at(p['changed'])}' " +
     ")\n"
+    
+    p['tags'].split(/\s?,\s?/).each do |t|
+      t = t.gsub(/\s+/," ").downcase
+      tvar = tag_var(t)
+      posts += "p#{i}.tags << #{tvar}\n"
+      tags += "#{tvar} = Tag.create(:name=>\"#{t}\")\n" unless tags_set.include? tvar
+      tags_set.add(t)
+    end
   end
+  f.write "# Tags\n"
+  f.write tags
+  f.write "\n# Posts\n"
+  f.write posts
+  
 end
