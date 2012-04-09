ee27a09d1b1527d19db66b9ee66e1ad47bdcfa7e
Adrian Mejia <axm7988@rit.edu>
2012-04-09 01:39:54 -0400

    autosave for tags and comments in Post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ActiveRecord::Base
   validates :title, :presence => true, :length => { :minimum => 2}
 
   belongs_to :user  
-  has_many :comments, dependent: :destroy
-  has_and_belongs_to_many :tags
+  has_many :comments, dependent: :destroy, :autosave => true
+  has_and_belongs_to_many :tags, :autosave => true
   
   accepts_nested_attributes_for :tags, allow_destroy: :true, 
     reject_if: proc{ |attrs| attrs.all?{|k,v| v.blank? } }
