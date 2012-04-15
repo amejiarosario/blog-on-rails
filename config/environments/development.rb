595fd7bfbb0575d97d712b0ece050eecba386ef2
Adrian Mejia <axm7988@rit.edu>
2012-04-14 22:17:37 -0400

    mailer setup with gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,21 @@ Blog::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  
+  # Mailer
+  config.action_mailer.default_url_options = { host: "adrianmejia.com" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "adrianmejia.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "adriansky@gmail.com",
+    password: "sbbcackk"
+  }
+  
+  
 end
