62dd66f240d782493fc2b07e5ee0b261abb16b07
Adrian Mejia <axm7988@rit.edu>
2012-04-25 17:01:09 -0400

    closes #8 Use an existing tag if it already exists instead of giving an error for duplicated tag.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   extend FriendlyId
+  before_validation :check_tags
   
   friendly_id :title, use: [:slugged, :history]
   
@@ -12,4 +13,28 @@ class Post < ActiveRecord::Base
   
   accepts_nested_attributes_for :tags, allow_destroy: :true, 
     reject_if: proc{ |attrs| attrs.all?{|k,v| v.blank? } }
+	
+	
+  private
+	# Use an existing tag if it already exists.
+	def check_tags
+		tags.map! do |tag|
+			existing_tag = Tag.find_by_name(tag.name)
+			(existing_tag)? existing_tag : tag
+		end
+	end
 end
+
+=begin
+	In a create operation under Rails, there are six callbacks before the database operation, and two after. In order, these are:
+
+	before_validation
+	before_validation_on_create
+	after_validation
+	after_validation_on_create
+	before_save
+	before_create
+	DATABASE INSERT
+	after_create
+	after_save
+=end
