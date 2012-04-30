7cfdee60276e53b528637c174bbc0a0b618fa9da
Adrian Mejia <axm7988@rit.edu>
2012-04-29 23:57:46 -0400

    mailer initializer added

--- config/application.rb
+++ config/application.rb
@@ -56,19 +56,5 @@ module Blog
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    # Mailer
-    config.action_mailer.default_url_options = { host: "adrianmejia.com" }
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: "adrianmejia.com",
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: "adriansky@gmail.com",
-      password: "sbbcackk"
-    }
-    
   end
 end
---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "adrianmejia.com",
+  :user_name            => ENV['GMAIL_USERNAME'],
+  :password             => ENV['GMAIL_PASSWORD'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
